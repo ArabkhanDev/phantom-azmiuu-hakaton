5e8125cc54bf0ee0a4b9e7403b0223da1000911b
arabkhanrmzv <arabkhanramazanov@gmail.com>
2026-04-16 11:21:27 +0400

    initialize project

---
+++ src/main/java/az/microservices/azmiuuhakaton/AzmiuuHakatonApplication.java
@@ -0,0 +1,13 @@
+package az.microservices.azmiuuhakaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzmiuuHakatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzmiuuHakatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/microservices/azmiuuhakaton/AzmiuuHakatonApplicationTests.java
@@ -0,0 +1,13 @@
+package az.microservices.azmiuuhakaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzmiuuHakatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
