e23f6fe44089fae109db7f7c600f82847be5964e
arabkhanrmzv <arabkhanramazanov@gmail.com>
2026-04-17 09:55:05 +0400

    fix: handle exceptions

--- src/main/java/az/microservices/azmiuuhakaton/exception/ErrorHandler.java
+++ src/main/java/az/microservices/azmiuuhakaton/exception/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
@@ -13,6 +14,13 @@ public class ErrorHandler {
     @ExceptionHandler(UsernameNotFoundException.class)
     @ResponseStatus(NOT_FOUND)
     public ErrorResponse handle(UsernameNotFoundException exception){
+        return new ErrorResponse(exception.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ErrorResponse handle(Exception exception){
         return new ErrorResponse(exception.getMessage());
     }
 }
